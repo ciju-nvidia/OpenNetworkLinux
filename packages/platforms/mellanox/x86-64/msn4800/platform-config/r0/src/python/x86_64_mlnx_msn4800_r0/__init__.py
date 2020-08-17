3a2904af2a34aef45517359b8d60360790b56b4f
Mykola Kostenok <c_mykolak@mellanox.com>
2020-08-17 09:57:17 +0300

    Add initial code for MSN4800.
    Signed-off-by: Mykola Kostenok <c_mykolak@mellanox.com>

---
+++ packages/platforms/mellanox/x86-64/msn4800/platform-config/r0/src/python/x86_64_mlnx_msn4800_r0/__init__.py
@@ -0,0 +1,16 @@
+from onl.platform.base import *
+from onl.platform.mellanox import *
+
+class OnlPlatform_x86_64_mlnx_msn4800_r0(OnlPlatformMellanox,
+                                               OnlPlatformPortConfig_128x100):
+    PLATFORM='x86-64-mlnx-msn4800-r0'
+    MODEL="MSN4800"
+    SYS_OBJECT_ID=".4800.1"
+
+    def baseconfig(self):
+        # load modules
+        import os
+        # necessary if there are issues with the install
+        # os.system("/usr/bin/apt-get install")
+        self.syseeprom_export();
+        return True
