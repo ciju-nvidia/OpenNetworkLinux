da714ad89132dcba1b7b99119cb2440f5f8d61d7
Mykola Kostenok <c_mykolak@nvidia.com>
2021-04-12 16:29:06 +0300

    platform: mellanox: Add new system E3597
    Add support for new Nvidia E3597 - LR10 Nvidia LR10 based NVL3 Switch,
     64 NVL3 ports, 32 OSFP ports, non-blocking switching capacity of 13.6Tbps,
     2 Power Supplies (AC), Standard depth, Managed, P2C airflow, Rail Kit, RoHS6.
    The rack switch is designed to provide high bandwidth, low latency NVLink
     connection between DGX products supporting external NVLink.
    
    Signed-off-by: Mykola Kostenok <c_mykolak@nvidia.com>

---
+++ packages/platforms/mellanox/x86-64/e3597/onlp/builds/x86_64_mlnx_e3597/module/inc/x86_64_mlnx_e3597/x86_64_mlnx_e3597_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_mlnx_e3597 Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_MLNX_E3597_DOX_H__
+#define __X86_64_MLNX_E3597_DOX_H__
+
+/**
+ * @defgroup x86_64_mlnx_e3597 x86_64_mlnx_e3597 - x86_64_mlnx_e3597 Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_mlnx_e3597-x86_64_mlnx_e3597 Public Interface
+ * @defgroup x86_64_mlnx_e3597-config Compile Time Configuration
+ * @defgroup x86_64_mlnx_e3597-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __X86_64_MLNX_E3597_DOX_H__ */
