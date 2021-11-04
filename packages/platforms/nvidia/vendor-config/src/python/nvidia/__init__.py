cc514a634f92db9c8d137dc087dd0070f7dd834e
Oleksandr Shamray <oleksandrs@nvidia.com>
2021-11-04 09:32:20 +0000

    platform: add syseeprom_export to Nvidia platform
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- packages/platforms/nvidia/vendor-config/src/python/nvidia/__init__.py
+++ packages/platforms/nvidia/vendor-config/src/python/nvidia/__init__.py
@@ -5,3 +5,16 @@ from onl.platform.base import *
 class OnlPlatformNvidia(OnlPlatformBase):
     MANUFACTURER='Nvidia'
     PRIVATE_ENTERPRISE_NUMBER=5703
+
+    #
+    # Some platforms rely on the output of the onie-syseeprom tool
+    # and the machine.conf file to implement parts of ONLP.
+    #
+    def syseeprom_export(self):
+        print "Caching ONIE System EEPROM..."
+        onie = self.onie_syseeprom_get()
+        mc = self.onie_machine_get()
+        # XXX roth -- deprecated
+        if 'onie_version' in mc:
+            onie['0x29'] = mc['onie_version']
+            self.onie_syseeprom_set(onie)
