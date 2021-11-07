0f67496295d4ce4811b2640f718bf6aa45d64765
Felix Radensky <fradensky@nvidia.com>
2021-11-07 10:20:57 +0000

    platform: mellanox: Add support for new system XH3000
     Add support for new system type XH3000, which is a water cooling
     Ethernet switch blade equipped with 32x200G Ethernet ports.

---
+++ packages/platforms/mellanox/x86-64/xh3000/onlp/builds/x86_64_bull_xh3000/module/inc/x86_64_bull_xh3000/x86_64_bull_xh3000_porting.h
@@ -0,0 +1,97 @@
+/**************************************************************************//**
+ *
+ * @file
+ * @brief x86_64_bull_xh3000 Porting Macros.
+ *
+ * @addtogroup x86_64_bull_xh3000-porting
+ * @{
+ *
+ *****************************************************************************/
+#ifndef __X86_64_BULL_XH3000_PORTING_H__
+#define __X86_64_BULL_XH3000_PORTING_H__
+
+
+/* <auto.start.portingmacro(ALL).define> */
+#if X86_64_BULL_XH3000_CONFIG_PORTING_INCLUDE_STDLIB_HEADERS == 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <memory.h>
+#endif
+
+#ifndef X86_64_BULL_XH3000_MALLOC
+    #if defined(GLOBAL_MALLOC)
+        #define X86_64_BULL_XH3000_MALLOC GLOBAL_MALLOC
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_MALLOC malloc
+    #else
+        #error The macro X86_64_BULL_XH3000_MALLOC is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_BULL_XH3000_FREE
+    #if defined(GLOBAL_FREE)
+        #define X86_64_BULL_XH3000_FREE GLOBAL_FREE
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_FREE free
+    #else
+        #error The macro X86_64_BULL_XH3000_FREE is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_BULL_XH3000_MEMSET
+    #if defined(GLOBAL_MEMSET)
+        #define X86_64_BULL_XH3000_MEMSET GLOBAL_MEMSET
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_MEMSET memset
+    #else
+        #error The macro X86_64_BULL_XH3000_MEMSET is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_BULL_XH3000_MEMCPY
+    #if defined(GLOBAL_MEMCPY)
+        #define X86_64_BULL_XH3000_MEMCPY GLOBAL_MEMCPY
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_MEMCPY memcpy
+    #else
+        #error The macro X86_64_BULL_XH3000_MEMCPY is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_BULL_XH3000_VSNPRINTF
+    #if defined(GLOBAL_VSNPRINTF)
+        #define X86_64_BULL_XH3000_VSNPRINTF GLOBAL_VSNPRINTF
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_VSNPRINTF vsnprintf
+    #else
+        #error The macro X86_64_BULL_XH3000_VSNPRINTF is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_BULL_XH3000_SNPRINTF
+    #if defined(GLOBAL_SNPRINTF)
+        #define X86_64_BULL_XH3000_SNPRINTF GLOBAL_SNPRINTF
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_SNPRINTF snprintf
+    #else
+        #error The macro X86_64_BULL_XH3000_SNPRINTF is required but cannot be defined.
+    #endif
+#endif
+
+#ifndef X86_64_BULL_XH3000_STRLEN
+    #if defined(GLOBAL_STRLEN)
+        #define X86_64_BULL_XH3000_STRLEN GLOBAL_STRLEN
+    #elif X86_64_BULL_XH3000_CONFIG_PORTING_STDLIB == 1
+        #define X86_64_BULL_XH3000_STRLEN strlen
+    #else
+        #error The macro X86_64_BULL_XH3000_STRLEN is required but cannot be defined.
+    #endif
+#endif
+
+/* <auto.end.portingmacro(ALL).define> */
+
+
+#endif /* __X86_64_BULL_XH3000_PORTING_H__ */
+/* @} */
---
+++ packages/platforms/mellanox/x86-64/xh3000/onlp/builds/x86_64_bull_xh3000/module/src/x86_64_bull_xh3000_log.c
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_bull_xh3000/x86_64_bull_xh3000_config.h>
+
+#include "x86_64_bull_xh3000_log.h"
+/*
+ * x86_64_bull_xh3000 log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      X86_64_BULL_XH3000_CONFIG_LOG_OPTIONS_DEFAULT,
+                      X86_64_BULL_XH3000_CONFIG_LOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      X86_64_BULL_XH3000_CONFIG_LOG_CUSTOM_BITS_DEFAULT
+                     );
+
