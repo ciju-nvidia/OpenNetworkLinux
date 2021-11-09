06d91f93c4e7782be8ba5ddf3106a9271f1d1d65
Felix Radensky <fradensky@nvidia.com>
2021-11-09 15:09:00 +0000

    platform: mellanox: Add support for new system XH3000
    Add support for new system type XH3000, which is a water cooling
    Ethernet switch blade equipped with 32x200G Ethernet ports.

---
+++ packages/platforms/mellanox/x86-64/xh3000/onlp/builds/x86_64_bull_xh3000/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_bull_xh3000 Autogeneration
+#
+###############################################################################
+x86_64_bull_xh3000_AUTO_DEFS := module/auto/x86_64_bull_xh3000.yml
+x86_64_bull_xh3000_AUTO_DIRS := module/inc/x86_64_bull_xh3000 module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/mellanox/x86-64/xh3000/onlp/builds/x86_64_bull_xh3000/module/inc/x86_64_bull_xh3000/x86_64_bull_xh3000_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_bull_xh3000 Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_BULL_XH3000_DOX_H__
+#define __X86_64_BULL_XH3000_DOX_H__
+
+/**
+ * @defgroup x86_64_bull_xh3000 x86_64_bull_xh3000 - x86_64_bull_xh3000 Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_bull_xh3000-x86_64_bull_xh3000 Public Interface
+ * @defgroup x86_64_bull_xh3000-config Compile Time Configuration
+ * @defgroup x86_64_bull_xh3000-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __X86_64_BULL_XH3000_DOX_H__ */
