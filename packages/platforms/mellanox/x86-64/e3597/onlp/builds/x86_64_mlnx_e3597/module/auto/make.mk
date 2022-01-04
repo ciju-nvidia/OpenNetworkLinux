c679b267daa64b704288ca582f942b3c46d1030f
Oleksandr Shamray <oleksandrs@nvidia.com>
2022-01-04 16:54:39 +0200

    Removed redundant e3597 under Mellanox vendor
    Signed-off-by: Oleksandr Shamray <oleksandrs@nvidia.com>

--- packages/platforms/mellanox/x86-64/e3597/onlp/builds/x86_64_mlnx_e3597/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# x86_64_mlnx_e3597 Autogeneration
-#
-###############################################################################
-x86_64_mlnx_e3597_AUTO_DEFS := module/auto/x86_64_mlnx_e3597.yml
-x86_64_mlnx_e3597_AUTO_DIRS := module/inc/x86_64_mlnx_e3597 module/src
-include $(BUILDER)/auto.mk
-
