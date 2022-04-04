f317bbcf28239a29b5846289b18262b5d93ebff9
Michael Shych <michaelsh@nvidia.com>
2022-04-04 22:59:39 +0300

    Add MSN5600 (Moose) system.
    Signed-off-by: Michael Shych <michaelsh@nvidia.com>

---
+++ packages/platforms/mellanox/x86-64/msn5600/onlp/builds/x86_64_mlnx_msn5600/module/auto/make.mk
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# x86_64_mlnx_msn5600 Autogeneration
+#
+###############################################################################
+x86_64_mlnx_msn5600_AUTO_DEFS := module/auto/x86_64_mlnx_msn5600.yml
+x86_64_mlnx_msn5600_AUTO_DIRS := module/inc/x86_64_mlnx_msn5600 module/src
+include $(BUILDER)/auto.mk
+
---
+++ packages/platforms/mellanox/x86-64/msn5600/onlp/builds/x86_64_mlnx_msn5600/module/src/x86_64_mlnx_msn5600_log.c
@@ -0,0 +1,18 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_mlnx_msn5600/x86_64_mlnx_msn5600_config.h>
+
+#include "x86_64_mlnx_msn5600_log.h"
+/*
+ * x86_64_mlnx_msn5600 log struct.
+ */
+AIM_LOG_STRUCT_DEFINE(
+                      X86_64_MLNX_MSN5600_CONFIG_LOG_OPTIONS_DEFAULT,
+                      X86_64_MLNX_MSN5600_CONFIG_LOG_BITS_DEFAULT,
+                      NULL, /* Custom log map */
+                      X86_64_MLNX_MSN5600_CONFIG_LOG_CUSTOM_BITS_DEFAULT
+                     );
+
